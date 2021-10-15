3779a5f131d2d39698c0d5f42b76845f5a46c6a5
Laura Moldovan <laura.duduma@gmail.com>
2021-10-15 23:11:56 +0200

    55

--- src/app/trips.ts
+++ src/app/trips.ts
@@ -5,54 +5,62 @@ export const trips = [
     startDate : "23.10.2021",
     duration : "7 Days",
     price : 1000,
-    img : "'./assets/img/travel/egipt.JPG'"
+    img : "./assets/img/travel/egipt.JPG"
 },{
     destination : "Greece",
     description : "Relax on the beautiful Coast of Santorini",
     startDate : "24.10.2021",
     duration : "5 Days",
     price : 500,
-    img : "'./assets/img/travel/santorini.jpg'"
+    img : "./assets/img/travel/santorini.jpg"
 },{
     destination : "Vienna",
     description : "A Week-end full of fun!",
     startDate : "25.10.2021",
     duration : "3 Days",
     price : 200,
-    img : "'./assets/img/travel/viena.JPG'"
+    img : "./assets/img/travel/viena.JPG"
 },{
     destination : "Paris",
     description : "Spend a Week in the Europe's most beautiful city.",
     startDate : "26.10.2021",
     duration : "7 Days",
     price : 600,
-    img : "'./assets/img/travel/paris2.jpg'"
+    img : "./assets/img/travel/paris2.jpg"
 },{
     destination : "Los Angeles",
     description : "Feel like a star! 2 Weeks at Hollywood!",
     startDate : "27.10.2021",
     duration : "14 Days",
     price : 3500,
-    img : "'./assets/img/travel/hollywood.jpg'"
+    img : "./assets/img/travel/hollywood.jpg"
 },{
     destination : "Machu Pichu",
     description : "A trip full of adventure waits for you",
     startDate : "28.10.2021",
     duration : "14 Days",
     price : 2500,
-    img : "'./assets/img/travel/egipt.JPG'"
+    img : "./assets/img/travel/macpic.jpg"
 },{
     destination : "London",
     description : "The best Trip ever!",
     startDate : "29.10.2021",
     duration : "5 Days",
     price : 450,
-    img : "'./assets/img/travel/macpic.jpg'"
+    img : "./assets/img/travel/london.jpg"
 },{
     destination : "Venetia",
     description : "Your best Halloween Party in Venetia",
     startDate : "30.10.2021",
     duration : "3 Days",
     price : 250,
-    img : "'./assets/img/travel/venetia.jpg'"
-}];
+    img : "./assets/img/travel/venetia.jpg"
+},{
+    destination : "Tokyo",
+    description : "Live your dream and become Yakuza",
+    startDate : "31.10.2021",
+    duration : "10 Days",
+    price : 1500,
+    img : "./assets/img/travel/tokyo.JPG"
+}
+];
